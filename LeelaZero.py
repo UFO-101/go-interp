Update with better visualization, gradient probe and many other improvements and fixes.

LeelaZero.py
@@ -73,30 +73,31 @@ class ConvBlock(nn.Module):
         # initializations
         nn.init.kaiming_normal_(self.conv.weight, mode="fan_out", nonlinearity="relu")
 
-    def forward(self, x):
-        x = self.conv(x)
-        x = self.bn(x)
-        x += self.beta.view(1, self.bn.num_features, 1, 1).expand_as(x)
-        return F.relu(x, inplace=True) if self.relu else x
+    def forward(self, input, leaky_relu: bool = False):
+        a = self.conv(input)
+        b = self.bn(a)
+        c = b + self.beta.view(1, self.bn.num_features, 1, 1).expand_as(b)
+        if leaky_relu:
+            return F.leaky_relu(c) if self.relu else c
+        else:
+            return F.relu(c) if self.relu else c
 
 
+# *Residualified* residual block
 class ResBlock(nn.Module):
-    def __init__(self, in_channels: int, out_channels: int, residualify: bool = False):
+    def __init__(self, in_channels: int, out_channels: int):
         super().__init__()
         self.conv1 = ConvBlock(in_channels, out_channels, 3)
         self.conv2 = ConvBlock(out_channels, out_channels, 3, relu=False)
-        self.residualify = residualify
 
-    def forward(self, x):
+    def forward(self, x, leaky_relu: bool = False):
         identity = x
-        out = self.conv1(x)
-        out = self.conv2(out)
-        if self.residualify:
-            return torch.where(identity + out > 0, out, -identity)
+        mid = self.conv1(x, leaky_relu=leaky_relu)
+        out = self.conv2(mid)
+        if leaky_relu:
+            return torch.where(identity + out > 0, out, -identity * 0.99), mid
         else:
-            out += identity
-            return F.relu(out, inplace=True)
-
+            return torch.where(identity + out > 0, out, -identity), mid
 
 class Network(nn.Module):
     def __init__(
@@ -105,13 +106,12 @@ class Network(nn.Module):
         in_channels: int,
         residual_channels: int,
         residual_layers: int,
-        residualify: bool=False,
     ):
         super().__init__()
         self.conv_input = ConvBlock(in_channels, residual_channels, 3)
         self.residual_tower = nn.Sequential(
             *[
-                ResBlock(residual_channels, residual_channels, residualify)
+                ResBlock(residual_channels, residual_channels)
                 for _ in range(residual_layers)
             ]
         )
@@ -122,34 +122,36 @@ class Network(nn.Module):
         self.value_conv = ConvBlock(residual_channels, 1, 1)
         self.value_fc_1 = nn.Linear(board_size * board_size, 256)
         self.value_fc_2 = nn.Linear(256, 1)
-        self.residualify = residualify
 
-    def forward(self, planes, get_layer_activations=None):
+    def forward(self, planes, leaky_relu: bool = False):
+        activations = []
+        block_activations = []
+
         # first conv layer
-        x = self.conv_input(planes)
-        if get_layer_activations == -1:
-            layer_activations = x
+        x = self.conv_input(planes, leaky_relu=leaky_relu)
+        activations.append(x.detach().clone())
 
         # residual tower
-        for i, layer in enumerate(self.residual_tower):
-            layer_output = layer(x)
-            if i == get_layer_activations:
-                layer_activations = layer_output
-            if self.residualify:
-                x = x + layer_output
-            else:
-                x = layer_output
+        for layer in self.residual_tower:
+            layer_output, intermediate = layer(x, leaky_relu=leaky_relu)
+            # block_activations.append(intermediate.detach().clone())
+            block_activations.append(layer_output.detach().clone())
+            x = x + layer_output
+            activations.append(x.detach().clone())
 
         # policy head
-        pol = self.policy_conv(x)
+        pol = self.policy_conv(x, leaky_relu=leaky_relu)
         pol = self.policy_fc(torch.flatten(pol, start_dim=1))
 
         # value head
-        val = self.value_conv(x)
-        val = F.relu(self.value_fc_1(torch.flatten(val, start_dim=1)), inplace=True)
+        val = self.value_conv(x, leaky_relu=leaky_relu)
+        if leaky_relu:
+            val = F.leaky_relu(self.value_fc_1(torch.flatten(val, start_dim=1)), inplace=True)
+        else:
+            val = F.relu(self.value_fc_1(torch.flatten(val, start_dim=1)), inplace=True)
         val = torch.tanh(self.value_fc_2(val))
 
-        return pol, val, None if get_layer_activations is None else layer_activations
+        return pol, val, torch.stack(activations), torch.stack(block_activations)
 
     def to_leela_weights(self, filename: str):
         """

load_leela_weights.py
@@ -8,8 +8,10 @@ from LeelaZero import Network
 import importlib
 
 #%%
+import LeelaZero
 importlib.reload(LeelaZero)
 from LeelaZero import Network
+import requests
 
 url = "https://zero.sjeng.org/best-network"
 # url = "https://github.com/yukw777/leela-zero-pytorch/raw/master/weights/leela-zero-pytorch-sm.txt"
@@ -55,58 +57,3 @@ network.from_leela_weights(filename)
 #%%
 # Save the network to a file
 torch.save(network.state_dict(), ".cache/best-network.pt")
-# %%
-
-# Old code I wrote to load from SGF. Probably redundant now.
-#%%
-with open("MeVsLeela.sgf", "rb") as f:
-    game = sgf.Sgf_game.from_bytes(f.read())
-board = boards.Board(game.get_size())
-
-black_tensors = []
-white_tensors = []
-color = 'b'
-for node in game.main_sequence_iter():
-    color, move = node.get_move()
-    if move is not None:
-        board.play(move[0], move[1], color)
-    board_array = np.array(board.board)
-    white_tensors.append(torch.from_numpy((board_array == 'w').astype(np.float32)))
-    black_tensors.append(torch.from_numpy((board_array == 'b').astype(np.float32)))
-
-input_tensors = []
-side_to_move_tensors = white_tensors if color == 'b' else black_tensors
-for i in range(8):
-    try:
-        input_tensors.append(side_to_move_tensors[-1 - i])
-    except IndexError:
-        input_tensors.append(torch.zeros(game.get_size(), game.get_size()))
-other_size_tensors = black_tensors if color == 'b' else white_tensors
-for i in range(8):
-    try:
-        input_tensors.append(other_size_tensors[-1 - i])
-    except IndexError:
-        input_tensors.append(torch.zeros(game.get_size(), game.get_size()))
-
-if color == 'b':
-    input_tensors.append(torch.zeros(game.get_size(), game.get_size()))
-    input_tensors.append(torch.ones(game.get_size(), game.get_size()))
-else:
-    input_tensors.append(torch.ones(game.get_size(), game.get_size()))
-    input_tensors.append(torch.zeros(game.get_size(), game.get_size()))
-
-input = torch.stack(input_tensors, dim=0).unsqueeze(0).to(device)
-print('input', input.shape)
-
-network.to(device)
-network.eval()
-pol, val = network(input)
-
-print('pol', pol)
-print('val', val)
-
-
-x, y = pol[0].argmax() % game.get_size(), pol[0].argmax() // game.get_size()
-print('best move - x:', x, 'y:', y)
-print('board value', val.item())
-# %%

parse_leela_data.py
@@ -121,8 +121,9 @@ class Dataset:
         move_probs: List[np.ndarray] = []
         outcomes: List[int] = []
         self.raw_datapoints: List[List[str]] = []
-        with ProcessPoolExecutor() as executor:
-            for data in executor.map(get_data_from_file, filenames):
+        # with ProcessPoolExecutor() as executor:
+            # for data in executor.map(get_data_from_file, filenames):
+        for data in map(get_data_from_file, filenames):
                 f_stone_planes, f_turn_planes, f_move_probs, f_outcomes = data
                 stone_planes.extend(f_stone_planes)
                 turn_planes.extend(f_turn_planes)

test.py
@@ -1,118 +1,95 @@
 #%%
 import torch
-from LeelaZero import Network
+import LeelaZero
 from parse_leela_data import Dataset
-import plotly.express as px
 import pandas as pd
 from einops import rearrange
+from torchviz import make_dot
+import plotly.express as px
 import importlib
+from utils import print_go_board_tensor, display_tensor_grid, tensor_symmetries, point_symmetries
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-memory_available = torch.cuda.get_device_properties(device).total_memory
-print("Device: ", device, "- Memory available: ", memory_available)
-
-def print_go_board_tensor(tensor, turn_number, show_n_turns_past=0, only_to_play=False, coords=None):
-    tensor = tensor[0]
-    color_to_play = 'b' if turn_number % 2 == 0 else 'w'
-    print("Color to play:", color_to_play)
-    for i in reversed(range(19)):
-        for j in range(19):
-            if coords is not None and (j, i) in coords:
-                black_symbol = "🔴"
-                white_symbol = "🟡"
-                empty_symbol = "❌"
-            else:
-                black_symbol = "🟤"
-                white_symbol = "⚪"
-                empty_symbol = "➕"
-            if tensor[0 + show_n_turns_past][i][j] == 1:
-                print(black_symbol, end="") if color_to_play == 'b' else print(white_symbol, end="")
-            elif tensor[8 + show_n_turns_past][i][j] == 1 and not only_to_play:
-                print(white_symbol, end="") if color_to_play == 'b' else print(black_symbol, end="")
-            else:
-                print(empty_symbol, end="")
-        print()
-
-def show_layer_activations(activations, boards_width=16, boards_height=16):
-    # output[0] has shape (16 * 16, 19, 19). Pad each 19x19 with a surrounding border of zeros.
-    print('activations shape:', activations.shape)
-    output_padded = torch.zeros((boards_width * boards_height, 20, 20)) - 1
-    output_padded[:, :-1, :-1] = activations.detach()[0] if activations.shape[0] == 1 else activations.detach()
-    output_padded = rearrange(output_padded, '(i j) h w -> (i h) (j w)', i=boards_width)
-    px.imshow(output_padded.cpu().numpy(), origin="lower", height=800).show()
-    
-
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 #%%
-import LeelaZero
 importlib.reload(LeelaZero)
-from LeelaZero import Network
-layer_count = 40
-channel_count = 256
-residualify = False
-network = Network(19, 18, channel_count, layer_count, residualify=residualify)
+layer_count, channel_count = 40, 256
+network = LeelaZero.Network(19, 18, channel_count, layer_count)
 network.load_state_dict(torch.load(".cache/best-network.pt"))
 network.eval()
 network.to(device)
-print("Network loaded.")
-
+print("LeelaZero.Network loaded.")
 #%%
-dataset = Dataset(["game-data/better-long-range-atari.txt.0.gz"])
-print("Number of input:", len(dataset))
-#%%
-turn_number = 50
-print_go_board_tensor(dataset[turn_number], turn_number, show_n_turns_past=0, only_to_play=False)
-
+pos_infos = [("leelaoutput", 294, (2, 11)), ("better-long-range-atari", 50, (9, 15)), ("spiral-atari-1", 162, (3, 9))]
+inputs = []
+points, point_indices = [], []
+for filename, turn, coords in pos_infos:
+    input = Dataset([f"game-data/{filename}.txt.0.gz"])[turn][0]
+    print_go_board_tensor(input, turn, coords=[coords])
+    inputs.append(tensor_symmetries(input))
+    points.extend(point_symmetries(*coords)[0])
+    point_indices.extend(point_symmetries(*coords)[1])
+inputs = torch.cat(inputs).to(device)
 #%%
 # SHOW ACTIVATIONS and POLICY
 network.eval()
-input = dataset[turn_number][0].unsqueeze(0).to(device).requires_grad_(False)
-print('input', input.shape)
+pol, val, activations, _ = network(inputs, leaky_relu=False)
+print('activations', activations.shape)
+display_tensor_grid(activations[:, 16], animate=True)
 
-def display_module_output(module, input, output):
-    show_layer_activations(output)
-    
-channel_to_view = 166
-channel_activations = []
-for i in range(layer_count):
-    # handle = network.residual_tower[i].register_forward_hook(display_module_output)
-    try:
-        with torch.inference_mode():
-            pol, val, activations = network(input, get_layer_activations=i)
-            channel_activations.append(activations[0, channel_to_view].detach().clone())
-    finally:
-        # handle.remove()
-        pass
-activations_combined = torch.stack(channel_activations)
-print("Activations across layers for channel", channel_to_view)
-show_layer_activations(activations_combined, boards_width=8, boards_height=5)
-
-print("POLICY")
+print("POLICY 0")
 px.imshow(pol[0][:-1].reshape(19, 19).cpu().detach().numpy(), origin="lower").show()
-print(f'max pol x:{pol[0].argmax() % 19}, y:{pol[0].argmax() // 19}.')
-print(f'Board value: {((1 + val.item()) / 2.0) * 100}%')
+print(f'Board value: {((1 + val[0].item()) / 2.0) * 100}%')
+print("POLICY 1")
+px.imshow(pol[1][:-1].reshape(19, 19).cpu().detach().numpy(), origin="lower").show()
+print("POLICY 10")
+px.imshow(pol[10][:-1].reshape(19, 19).cpu().detach().numpy(), origin="lower").show()
 
 #%%
+# GRADIENT PROBE
+grad_network = LeelaZero.Network(19, 18, channel_count, layer_count)
+grad_network.load_state_dict(torch.load(".cache/best-network.pt"))
+grad_network.train()
+for module in grad_network.modules():
+    if isinstance(module, torch.nn.BatchNorm2d):
+        module.eval()
 
-# print('network.conv_input.conv.weight', network.conv_input.conv.weight.shape)
-# px.imshow(network.conv_input.conv.weight[166].detach().cpu().numpy(), origin="lower", facet_col=0, facet_col_wrap=4).show()
-resid_block = 0
-print(f'network.residual_tower[{resid_block}].conv1.conv.weight', network.residual_tower[resid_block].conv1.conv.weight.shape)
-# weights_to_plot = network.residual_tower[resid_block].conv1.conv.weight
-weights_to_plot = network.conv_input.conv.weight
-channel_to_plot = 166
+optimizer = torch.optim.Adam(grad_network.parameters(), lr=0.00001)
+grad_network.zero_grad()
+pol, val, activations, block_activations = grad_network(inputs)
+point_pols = torch.gather(pol, dim=-1, index=torch.tensor(point_indices).unsqueeze(-1))
+loss = -point_pols.mean()
+print('loss', loss)
+loss.backward()
+optimizer.step()
+pol, val, mod_activations, mod_block_outputs = grad_network(inputs)
+activation_diff = mod_activations - activations
+block_output_diff = mod_block_outputs - block_activations
+print("activation diff", activation_diff.shape)
+display_tensor_grid(activation_diff[:, 0], title="activation 1 grad step diff", animate=True)
+
+# print("negative weight gradients")
+# display_tensor_grid(-grad_network.residual_tower[26].conv2.conv.weight.grad)
+# print("weights")
+# display_tensor_grid(grad_network.residual_tower[39].conv2.conv.weight[83])
+#%%
+# display weights
+
+weights_to_plot = network.residual_tower[39].conv2.conv.weight
+# weights_to_plot = network.conv_input.conv.weight
+channel_to_plot = 56
 print('weights_to_plot', weights_to_plot.shape)
 fig = px.imshow(weights_to_plot[channel_to_plot].detach().cpu().numpy(), origin="lower", facet_col=0, facet_col_wrap=16, facet_col_spacing=0.001, facet_row_spacing=0.01, width=1000, height=1000)
 fig.for_each_annotation(lambda a: a.update(text=a.text.split("_")[-1]))
 fig.show()
 
 #%%
-# ACTIVATION PATCHING
+# activation patching setup
 atari_turn = 50
-print_go_board_tensor(dataset[atari_turn], atari_turn, show_n_turns_past=0, only_to_play=False)
+print_go_board_tensor(dataset[atari_turn], atari_turn, show_n_turns_past=0, only_to_play=false)
 safe_turn = 54
-print_go_board_tensor(dataset[safe_turn], safe_turn, show_n_turns_past=0, only_to_play=False)
-atari_input = dataset[atari_turn][0].unsqueeze(0).to(device).requires_grad_(False)
-safe_input = dataset[safe_turn][0].unsqueeze(0).to(device).requires_grad_(False)
+print_go_board_tensor(dataset[safe_turn], safe_turn, show_n_turns_past=0, only_to_play=false)
+atari_input = dataset[atari_turn][0].unsqueeze(0).to(device).requires_grad_(false)
+safe_input = dataset[safe_turn][0].unsqueeze(0).to(device).requires_grad_(false)
 atari_pol_unpatched, _, _ = network(atari_input)
 safe_pol_unpatched, _, _ = network(safe_input)
 atari_escape_coords = (9, 15)
@@ -125,7 +102,7 @@ def plot_atari_escape_pol(*args, **kwargs):
     fig.show()
 
 #%%
-# LAYER PATCHING
+# layer patching
 network.eval()
 layer_patching_vals = []
 for layer_index in range(layer_count):
@@ -169,57 +146,67 @@ plot_atari_escape_pol(layer_and_channel_patching_vals, labels={'x': 'Layer', 'y'
 
 #%%
 # TRAIN the INPUT to maximize a neuron's activation
+
 class ParseTrainedInput(torch.nn.Module):
     def __init__(self):
         super().__init__()
     def forward(self, x):
-        # x = (torch.nn.functional.relu(x) * 2) - 1
-        input = torch.zeros((1, 18, 19, 19), device=device)
-        input[0, 0] = x.clamp(min=0, max=1)
-        input[0, 8] = -x.clamp(max=0, min=-1)
-        input[0, 15] = 1
-        # Round anything above 0.8 to 1.0 and anything below to 0.0
+        x = torch.nn.functional.tanh(x)
+        layer_0 = torch.maximum(x, torch.zeros_like(x)).unsqueeze(0)
+        layers_1_7 = torch.zeros_like(x).repeat(7, 1, 1)
+        layer_8 = torch.maximum(-x, torch.zeros_like(x)).unsqueeze(0)
+        layers_9_15 = torch.zeros_like(x).repeat(7, 1, 1)
+        layer_16 = torch.full_like(x, fill_value=torch.max(x).item()).unsqueeze(0)
+        # layer_16 = torch.ones_like(x).unsqueeze(0)
+        layer_17 = torch.zeros_like(x).unsqueeze(0)
+        input = torch.cat((layer_0, layers_1_7, layer_8, layers_9_15, layer_16, layer_17), dim=0).unsqueeze(0)
         threshold = 0.8
         rounded_input = torch.where(input > threshold, torch.ones_like(input), torch.zeros_like(input))
         return input, rounded_input
 
-trained_input = torch.randn((19, 19), requires_grad=True, device=device)
+# class ParseTrainedInput(torch.nn.Module):
+#     def __init__(self):
+#         super().__init__()
+#     def forward(self, x):
+#         x = (torch.nn.functional.tanh(x) + 1) / 2
+#         x = torch.cat([x, torch.ones((1, 1, 19, 19)), torch.zeros((1, 1, 19, 19))], dim=1)
+#         return x
+
+trained_input = torch.full((19, 19), 0.0, requires_grad=True, device=device) #, fill_value=0.0)
+# trained_input = torch.full((1, 16, 19, 19), fill_value=-2.0, requires_grad=True, device=device)
 parser = ParseTrainedInput()
-optimizer = torch.optim.Adam([trained_input], lr=0.15)
-neuron_layer, neuron_channel = -1, 2 # layer -1 for input_cov
+optimizer = torch.optim.Adam([trained_input], lr=0.2)
+neuron_layer, neuron_channel = 25, 166 # layer -1 for input_cov
 # coords = torch.tensor([(2, 15), (3, 15), (4, 15), (5, 15)], dtype=torch.long, device=device)
+# 1.4892 - 1.4893
+
+
 coords = [(2, 15)]
 coords_tensor = torch.tensor(coords, dtype=torch.long, device=device)
 loss_history = []
 torch.autograd.set_detect_anomaly(True)
+network.eval()
 
-network.train()
+print(f"Optimising for layer {neuron_layer}, channel {neuron_channel}, coords {coords}")
 for i in range(100):
     optimizer.zero_grad()
     input, rounded_input = parser(trained_input)
-    # handle = network.residual_tower[neuron_layer].register_forward_hook(display_module_output)
-    try:
-        pol, val, activations = network(input, neuron_layer)
-    finally:
-        # handle.remove()
-        pass
-    # Sum the activations of coordinates in coords
-    channel_activations = -activations[0, neuron_channel]
-    coords_activations = channel_activations[tuple(coords_tensor)]
-    loss = torch.sum(coords_activations)
+    # input = parser(trained_input)
+    pol, val, activations = network(input, neuron_layer, leaky_relu=False)
+    channel_activations = activations[0, neuron_channel]
+    coords_activations = channel_activations[tuple(coords_tensor[:, 1]), tuple(coords_tensor[:, 0])]
+    # other_activations = torch.sum(activations) - torch.sum(coords_activations)
+    loss = -torch.sum(coords_activations)
+    # other_activations = torch.sum(activations) - torch.sum(coords_activations)
+    # loss = other_activations * 0.01 - torch.mean(coords_activations)
     loss_history.append(loss.item())
     loss.backward()
     optimizer.step()
 
-# print(f"Layer {neuron_layer} ACTIVATIONS (optimizing neuron {neuron_channel}, {neuron_x}, {neuron_y})")
-show_layer_activations(activations)
-# print("TRAINED INPUT")
-# px.imshow(trained_input.detach().cpu().numpy(), origin="lower").show()
-# print("Parsed trained input")
-# px.imshow(input.detach().cpu().numpy()[0, 0], origin="lower").show()
-# px.imshow(input.detach().cpu().numpy()[0, 8], origin="lower").show()
-print("Rounded parsed trained input")
+display_tensor_grid(activations)
+print("TRAINED INPUT")
+px.imshow(trained_input.detach().cpu().numpy(), origin="lower").show()
 print_go_board_tensor(rounded_input, 0, coords=coords)
+# show_layer_activations(parser(trained_input), boards_width=4, boards_height=5)
 px.line(loss_history, height=300).show()
-
 # %%