351e58decd8cf64dabf8e4ea7adfedd3af2e2716
UFO-101 <josephmiller101@gmail.com>
2023-06-29 00:24:21 +0100

    Update data parsing code to make it more resilient on big datasets (ie. output one file at a time for each input file).

--- gather_data.py
+++ gather_data.py
@@ -8,17 +8,15 @@ from utils import display_tensor_grid, print_go_board_tensor
 import plotly.express as px
 import time
 import os
+from tqdm import tqdm
 from multiprocessing import Pool, cpu_count
 from torch.utils.data import Dataset, DataLoader, random_split
+from functools import partial
 #%%
 
 t.set_printoptions(linewidth=200)
 
 start_time = time.time()
-
-PATH = "/Users/josephmiller/Downloads/ladder-with-breaker.sgf"
-# PATH = "/Users/josephmiller/Downloads/connection_test_realgame2_B.sgf"
-# PATH = "/Users/josephmiller/Downloads/connection_test_realgame2_A.sgf"
 
 def board_state_to_2d_tensors(board):
     board_size = board.side
@@ -129,7 +127,15 @@ def file_to_data(path):
     games = []
     with open(path, "r") as f:
         for line_str in f:
-            games.append(sgf.Sgf_game.from_string(line_str))
+            try:
+                game = sgf.Sgf_game.from_string(line_str)
+                if game.get_size() == 19:
+                    games.append(game)
+            except ValueError as e:
+                if "bad SZ property" in str(e) or "no SGF data found" in str(e):
+                    pass
+                else:
+                    raise e
 
     boards, leela_inputs = [], []
     for game in games:
@@ -165,36 +171,34 @@ def file_to_data(path):
     # px.imshow(cyclic_tensors[0], origin="lower").show()
     return leela_inputs, liberties_tensors, cyclic_tensors
 
-end_time = time.time()
-
-def process_file(file_path):
+def process_file(file_path, out_dir):
     inputs, liberties, cycles = file_to_data(file_path)
-    if file_path.endswith(".sgf") or file_path.endswith(".sgfs"):
-        return (file_path, [inputs, liberties, cycles])
-    else:
-        return None, None
+    file_name = os.path.basename(file_path)
+    t.save([inputs, liberties, cycles], os.path.join(out_dir, file_name + ".pt"))
 
 def main(input_dir, output_dir):
     pool = Pool(cpu_count())  # Create a multiprocessing Pool
     file_paths = [os.path.join(root, file) for root, _, files in os.walk(input_dir) for file in files]
     print("File paths len", len(file_paths))
     print("File paths", file_paths[:10])
-    results = pool.map(process_file, file_paths)
+    # results = pool.map(process_file, file_paths)
+    process_file_and_save = partial(process_file, out_dir=output_dir)
+    list(tqdm(pool.imap(process_file_and_save, file_paths), total=len(file_paths)))
     
-    tensor_dict = {file_path: tensors for file_path, tensors in results if tensors is not None}
+    # tensor_dict = {file_path: tensors for file_path, tensors in results if tensors is not None}
     
-    print("Tensor dict len", len(tensor_dict))
-    print("tensor dict keys", tensor_dict.keys())
-    # Write tensor_dict to file in output_dir
-    t.save(tensor_dict, os.path.join(output_dir, 'tensor_dict.pt'))
+    # print("Tensor dict len", len(tensor_dict))
+    # print("tensor dict keys", tensor_dict.keys())
+    # # Write tensor_dict to file in output_dir
+    # t.save(tensor_dict, os.path.join(output_dir, 'tensor_dict.pt'))
 
 # input_dir = "/Users/josephmiller/Downloads/"
 # output_dir = "/Users/josephmiller/Documents/go-interp/parsed_data"
 # main(input_dir, output_dir)
 
 if __name__ == "__main__":
-    input_dir = "/Users/josephmiller/Documents/FAR/cp505-vs-cp103/sgfs"
-    output_dir = "/Users/josephmiller/Documents/go-interp/parsed_data"
+    input_dir = "/data/victimplay/ttseng-avoid-pass-alive-coldstart-39-20221025-175949/selfplay"
+    output_dir = "/weka/linear-probing/goattack-training-parsed"
     main(input_dir, output_dir)
 
 # print("Game len", game_len, "Sample len", sample_len)
